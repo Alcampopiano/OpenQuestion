22dbb6a463b68c0ad5cd22bc40a23722066be338
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2020-07-14 15:25:46 +0000

    Saved automatically

--- client_code/build/branch/confirm_content/__init__.py
+++ client_code/build/branch/confirm_content/__init__.py
@@ -10,14 +10,42 @@ class confirm_content(confirm_contentTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run when the form opens.
-
   def add_click(self, **event_args):
-    """This method is called when the Button is shown on the screen"""
-    pass
-
+    
+    items=self.flow_panel_build_condition.get_components()
+    
+    show_label=Label(text=items[0].selected_value)
+    if_label=Label(text='if', align='center')
+    widget_label=Label(text=items[2].selected_value)
+    # is_label=Label(text='if', align='center')
+    oper_label=Label(text=items[3].selected_value)
+    
+    if str(type(items[4])) is "<class 'anvil.Label'>":
+      val_text=items[4].text 
+    else:
+      val_text=items[4].selected_value
+      
+    val_label=Label(text=val_text)
+    minus_but=Button(icon='fa:minus')
+    minus_but.set_event_handler('click', self.minus_click)
+    
+    cond_flow=FlowPanel()
+    cond_flow.add_component(show_label)   
+    cond_flow.add_component(widget_label)
+    cond_flow.add_component(if_label)
+    cond_flow.add_component(oper_label)
+    cond_flow.add_component(val_label)
+    cond_flow.add_component(minus_but)
+    self.column_panel.add_component(cond_flow)
+    
+  def minus_click(self, **event_args):
+    print('minus')
+    
   def widget_change(self, **event_args):
     """This method is called when an item is selected"""
+    
+    if len(self.flow_panel_build_condition.get_components())>5:
+      self.flow_panel_build_condition.get_components()[-1].remove_from_parent()
     
     if self.drop_down_widgets.selected_value.__name__ in \
       ('drop_down', 'radio_button'):
@@ -27,8 +55,14 @@ class confirm_content(confirm_contentTemplate):
       self.flow_panel_build_condition.add_component(val_comp)
       
     elif self.drop_down_widgets.selected_value.__name__ == 'date':
-      val_comp=DatePicker()
+      val_comp=DatePicker(placeholder='choose a date')
       self.flow_panel_build_condition.add_component(val_comp)
+      
+    else:
+      val_comp=TextBox(type='number', placeholder='type number')
+      self.flow_panel_build_condition.add_component(val_comp)
+
+      
       
 
 
