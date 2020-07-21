b6afcba46ee4747d34067eefbca6cba3a133bb63
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2020-07-21 14:08:04 +0000

    Saved automatically

--- client_code/reports/widgets/chart_utilities/channel/__init__.py
+++ client_code/reports/widgets/chart_utilities/channel/__init__.py
@@ -4,6 +4,11 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+#import reports.widgets.chart_utilities.channel.bin as mybin
+from ..bin import bin
+from ..sort import sort
+from ..axis import axis
+from ..legend import legend
 
 class channel(channelTemplate):
   def __init__(self, **properties):
@@ -15,3 +20,31 @@ class channel(channelTemplate):
     
     if properties['channel_name']=='color':
       self.button_legend.visible=True
+
+  def button_bin_click(self, **event_args):
+    
+    bins_content=bin()
+    c=confirm(bins_content, large=True, 
+              buttons=[('apply', 'apply'),('cancel', 'cancel')],
+                      title="Bin properties")
+    
+  def button_sort_click(self, **event_args):
+    
+    sort_content=bin()
+    c=confirm(sort_content, large=True, 
+              buttons=[('apply', 'apply'),('cancel', 'cancel')],
+                      title="Sort properties")
+    
+  def button_axis_click(self, **event_args):
+    
+    axis_content=axis()
+    c=confirm(axis_content, large=True, 
+              buttons=[('apply', 'apply'),('cancel', 'cancel')],
+                      title="Axis properties")
+    
+  def button_legend_click(self, **event_args):
+    
+    legend_content=bin()
+    c=confirm(legend_content, large=True, 
+              buttons=[('apply', 'apply'),('cancel', 'cancel')],
+                      title="Legend properties")
