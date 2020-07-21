47df6597641ff4873c51b7bba562e488d077edec
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2020-07-21 16:39:09 +0000

    Saved automatically

--- server_code/server_reports.py
+++ server_code/server_reports.py
@@ -6,17 +6,21 @@ import altair as alt
 import pandas as pd
 
 @anvil.server.callable
-def make_chart():
+def make_chart(chart_dict):
   
-    source = pd.DataFrame({
+    df = pd.DataFrame({
         'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
         'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
     })
+    
+    marks={'bar': alt.mark_bar, 'line': alt.mark_line, 'area': alt.mark_area}
     
     c=alt.Chart(source).mark_bar(tooltip=True).encode(
         x='a',
         y='b'
     ).properties(width=500)
+    
+    
         
     #print(c.to_dict())
     return c.to_dict()
