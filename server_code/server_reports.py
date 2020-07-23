3b40996379acaf8092bbad791cc74e8d932c2b73
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2020-07-23 13:43:03 +0000

    Saved automatically

--- server_code/server_reports.py
+++ server_code/server_reports.py
@@ -18,11 +18,40 @@ def get_columns(file):
 def make_chart(spec, dataset):
   
     df=pd.read_csv(io.BytesIO(dataset.get_bytes()), index_col=0)
-    
     df_dicts=df.reset_index().to_dict('records')
     
-    spec["$schema"]="https://vega.github.io/schema/vega-lite/v4.json"
-    spec['data']={'name': 'data'}
-    spec['datasets']={spec['data']['name']: df_dicts}
+    spec=adjust_spec(spec)
     
     return spec
+  
+  
+  
+def adjust_spec(spec):
+  
+  for k in spec:
+    
+    if type(spec[k]) is dict and spec[k] != {}:
+            
+      adjust(spec[k])
+      
+    elif spec[k] == '' or spec[k] == {}:
+      
+      del spec[k]
+      
+      
+      #prop=properties(prop_text=k, spec_comp=spec[k])
+      #parent.column_panel.add_component(prop)
+      #self.tag.comp_list.append(prop)
+  
+
+  spec["$schema"]="https://vega.github.io/schema/vega-lite/v4.json"
+  spec['data']={'name': 'data'}
+  spec['datasets']={spec['data']['name']: df_dicts}
+  
+  
+  
+  
+  
+  
+  
+  
