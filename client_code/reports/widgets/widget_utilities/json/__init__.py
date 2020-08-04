6c9cdc0447eb6f6f317cf3bf514285a4d8a1359a
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2020-08-04 20:32:24 +0000

    Saved automatically

--- client_code/reports/widgets/widget_utilities/json/__init__.py
+++ client_code/reports/widgets/widget_utilities/json/__init__.py
@@ -9,13 +9,19 @@ class json(jsonTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.tag.form_shown=False
 
   def on_editor_change(self, spec):
     #self.tag.chart.vl_spec=spec
     self.tag.chart.tag.vl_spec=spec
-    self.tag.chart.form_show()
+    self.tag.chart.vega_embed()
     
   def form_show(self, **event_args):
-    spec=self.tag.chart.tag.vl_spec
-    #self.call_js("get_schema", spec, self, self.tag.parent)
-    self.call_js("make_editor", spec, self, self.tag.parent)
+
+    if not self.tag.form_shown:
+      print('here')
+      self.tag.form_shown=True
+      spec=self.tag.chart.tag.vl_spec
+      #self.call_js("get_schema", spec, self, self.tag.parent)
+      self.call_js("make_editor", spec, self, self.tag.parent)
+            
--- client_code/reports/widgets/widget_utilities/vega_lite/__init__.py
+++ client_code/reports/widgets/widget_utilities/vega_lite/__init__.py
@@ -9,6 +9,8 @@ class vega_lite(vega_liteTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+    self.tag.form_shown=False
 
     self.tag.vl_spec={
     "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
@@ -24,15 +26,23 @@ class vega_lite(vega_liteTemplate):
   
   def form_show(self, **event_args):
     
+    if not self.tag.form_shown:
+      self.tag.form_shown=True
+      self.vega_embed()
+      
+  def vega_embed(self, **event_args):
+    
     spec=self.tag.vl_spec
     data_name=spec['data'].get('name', None)
     data_values=get_open_form().tag.data_dicts.get(data_name, None)
     
     if data_name and data_values:
       self.call_js('vega_embed_named_data', self.tag.vl_spec, 
-                 data_name, data_values)
+                data_name, data_values)
       
     else: #not data_name and not data_values:
         self.call_js('vega_embed_no_named_data', self.tag.vl_spec)
-      
+        
+    
+        
     
