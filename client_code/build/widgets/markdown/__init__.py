412dff2f480c2bdb5cf800f53575e44527179376
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2020-08-05 18:45:47 +0000

    Saved automatically

--- client_code/build/widgets/markdown/__init__.py
+++ client_code/build/widgets/markdown/__init__.py
@@ -16,5 +16,22 @@ class markdown(markdownTemplate):
     from ..toolbar import toolbar
     toolbar=toolbar(align='left', section=section, parent=self)
     self.add_component(toolbar)
+    
+    augment.set_event_handler(self.column_panel_outer, 'click', 
+                              self.column_panel_outer_click)
+
+  def text_area_lost_focus(self, **event_args):
+    
+    text=self.text_area_text.text
+    
+    if text:
+      html=anvil.server.call('convert_markdown',text)
+      self.markdown_display.html=html
+      self.column_panel_outer.visible=True
+      self.text_area_text.visible=False
+    
+  def column_panel_outer_click(self, **event_args):
+    self.text_area_text.visible=True
+    self.column_panel_outer.visible=False
 
 
