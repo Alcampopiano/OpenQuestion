d1d0b6776947fce538c7dfef967c413b814a0c90
alcampopiano <alcampopiano@gmail.com>
2021-02-09 22:21:57 -0500

    tests added

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -4,39 +4,8 @@ import time
 from server_code.server_surveys import *
 import anvil.server
 import pytest
-
-
-@pytest.fixture(scope="session", autouse=True)
-def start_up_and_tear_down():
-
-  """
-  Everything before "yield" is run before any tests
-  Everything after "yield" is run after all tests have finished
-  """
-
-  # kill process on port in case it is running
-  os.system("fuser -k 3030/tcp")
-
-  # func to call app server
-  def start_server():
-    os.system("anvil-app-server --app ../OpenQuestion --uplink-key 42 --port 3030")
-
-  # start app server on a thread, allowing the rest of the script to run
-  threading.Thread(target=start_server).start()
-
-  # give time for the web server to spin up before continuing
-  time.sleep(60)
-
-  # connect
-  anvil.server.connect('42', url="ws://localhost:3030/_/uplink")
-
-  yield True
-
-  # disconnect from uplink
-  anvil.server.disconnect()
-
-  # kill process on that port
-  os.system("fuser -k 3030/tcp")
+import uuid
+from datetime import datetime
 
 # basic survey
 schema={
@@ -67,14 +36,78 @@ schema={
   ]
 }
 
+# id
+form_id = str(uuid.uuid4())
+
+@pytest.fixture(scope="session", autouse=True)
+def set_up_and_tear_down():
+
+  """
+  Everything before "yield" is run before any tests
+  Everything after "yield" is run after all tests have finished
+  """
+
+  # kill process on port in case it is running
+  os.system("fuser -k 3030/tcp")
+
+  # func to call app server
+  def start_server():
+    os.system("anvil-app-server --app ../OpenQuestion --uplink-key 42 --port 3030")
+
+  # start app server on a thread, allowing the rest of the script to run
+  threading.Thread(target=start_server).start()
+
+  # give time for the web server to spin up before continuing
+  time.sleep(60)
+
+  # connect
+  anvil.server.connect('42', url="ws://localhost:3030/_/uplink")
+
+  # add a survey
+  app_tables.forms.add_row(form_id=form_id, last_modified=datetime.now(),
+                           schema=schema, title=schema['title'])
+
+  yield True
+
+  # disconnect from uplink
+  anvil.server.disconnect()
+
+  # kill process on that port
+  os.system("fuser -k 3030/tcp")
+
+def test_delete_survey():
+
+  # current number of surveys
+  init_num_forms = len(app_tables.forms.search())
+
+  # add a survey to the database
+  delete_survey(form_id)
+
+  # a simple assertion that there is one more survey in the database
+  assert init_num_forms > len(app_tables.forms.search())
+
 def test_save_schema():
 
   # current number of surveys
-  cur_num_forms=len(app_tables.forms.search())
+  init_num_forms=len(app_tables.forms.search())
 
   # add a survey to the database
   save_schema(None, schema)
 
   # a simple assertion that there is one more survey in the database
-  assert cur_num_forms < len(app_tables.forms.search())
+  assert init_num_forms < len(app_tables.forms.search())
 
+def test_save_survey_settings():
+
+  row=app_tables.forms.add_row(form_id=form_id, schema=schema,
+                           title=schema['title'])
+
+  settings_in_schema={'survey_color': '#F0F0F0',
+                   'thank_you_msg': '#Thank you!'}
+
+  settings_in_datatable={'opening_date': None,
+                         'closing_date': None}
+
+  save_survey_settings(form_id, settings_in_schema, settings_in_datatable)
+
+  assert schema['settings'] != row['schema']['settings']
