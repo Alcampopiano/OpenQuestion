6654918aceabc4789d7228216aa334675524d33a
campopianoa@hcdsb.org <campopianoa@hcdsb.org>
2021-02-25 19:41:52 +0000

    Edited reports.widgets.widget_utilities.gen_chart_params

--- client_code/reports/widgets/widget_utilities/gen_chart_params/__init__.py
+++ client_code/reports/widgets/widget_utilities/gen_chart_params/__init__.py
@@ -27,6 +27,9 @@ class gen_chart_params(gen_chart_paramsTemplate):
     data_dicts=get_open_form().tag.data_dicts
     cols=data_dicts[name][0].keys()
     self.drop_down_columns.items=cols
+    self.flow_panel_columns.tag.current_cols=[]
+    [c.remove_from_parent() for c in self.flow_panel_columns.get_components()]
+    
     
   def drop_down_dataset_change(self, **event_args):
     """This method is called when an item is selected"""
@@ -36,14 +39,16 @@ class gen_chart_params(gen_chart_paramsTemplate):
     """This method is called when an item is selected"""
     value=self.drop_down_columns.selected_value
     
-    if value:
+    if value and value not in self.flow_panel_columns.tag.current_cols:
       b=Button(text=value, role='primary-color', font_size=12, icon='fa:times-circle')
       b.set_event_handler('click', self.but_click)
       self.flow_panel_columns.add_component(b)
       self.flow_panel_columns.tag.current_cols.append(value)
       
   def but_click(self, **event_args):
+    self.flow_panel_columns.tag.current_cols.remove(event_args['sender'].text)
     event_args['sender'].remove_from_parent()
+    
       
       
       
