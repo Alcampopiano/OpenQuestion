bafa6eb698dbaec202caa994c79284d5ca8556b4
alcampopiano <alcampopiano@gmail.com>
2021-03-22 13:19:44 -0400

    features for automatic charts and ver update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(path, encoding="utf-8"):
 
 setup(
     name='OpenQuestion',
-    version='0.0.9',
+    version='0.0.91',
     description='An open source survey platform written in Python.',
     author='Allan Campopiano',
     author_email="campopianoa@hcdsb.org",
